7962560f115ccc2369f62e000c9fde5aa6097f5e
Peter Perhac <perhac.peter@gmail.com>
2018-12-20 21:33:11 +0000

    started on bob exercise

--- haskell/queen-attack/src/Queens.hs
+++ haskell/queen-attack/src/Queens.hs
@@ -1,7 +1,13 @@
 module Queens (boardString, canAttack) where
 
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString white black = error "You need to implement this function."
+boardString white black =
+  do row <- [1..8]
+     col <- [1..9]
+     return c col row
+  where c :: Int -> Int -> Char
+        c 9 y = '\n'
+        c x y = '-'
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
 canAttack queenA queenB = error "You need to implement this function."
