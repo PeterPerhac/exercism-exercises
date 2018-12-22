e3d605599dae95d985b77f0e9d6a933ab7f3366d
Peter Perhac <perhac.peter@gmail.com>
2018-12-22 16:56:25 +0000

    wip on multiple groups

--- scala/book-store/src/main/scala/BookStore.scala
+++ scala/book-store/src/main/scala/BookStore.scala
@@ -1,45 +1,46 @@
+import BookWithPrice.bookWithDiscount
+
 case class BookWithPrice(id: Int, price: Int) {
   lazy val withoutDiscount: BookWithPrice = BookWithPrice(id, BookWithPrice.defaultBookPrice)
 }
+
 object BookWithPrice {
   val defaultBookPrice: Int = 800
-  val create: Int => BookWithPrice = BookWithPrice(_, defaultBookPrice)
-  val discounted: Int => (Int => Int) => BookWithPrice =
-    id => discount => BookWithPrice(id, discount(defaultBookPrice))
+
+  def book(id: Int): BookWithPrice = BookWithPrice(id, defaultBookPrice)
+
+  def bookWithDiscount(id: Int, discountApplicator: Int => Int): BookWithPrice =
+    BookWithPrice(id, discountApplicator(defaultBookPrice))
 }
 
 object BookStore {
 
   def total(items: List[Int]): Int =
-    groupPrice(items)
+    items.combinations(math.min(items.size, 5)).map {
+      foo =>
+        println(foo)
+        foo
+    }.map(groupPrice).map(i => {
+      println(i); i
+    }).min
 
-  val distinctBooksInGroup: List[Int] => Int =
-    _.distinct.size
-
-  val discountFactor: Int => Double = {
-    case 1 => 1.00
-    case 2 => 0.95
-    case 3 => 0.90
-    case 4 => 0.80
-    case _ => 0.75
-  }
+  val discountFactor: Map[Int, Double] = Map(1 -> 1.00, 2 -> 0.95, 3 -> 0.90, 4 -> 0.80).withDefaultValue(0.75)
 
   val roundMoney: Double => Int =
     d => (math rint d).toInt
 
   val discount: Int => Int => Int =
-    distinctBooks => ((_: Int) * discountFactor(distinctBooks)) andThen roundMoney
+    distinctBooks => originalPrice =>
+      roundMoney(originalPrice * discountFactor(distinctBooks))
 
-  val priceBookGroup: List[Int] => List[BookWithPrice] =
-    group =>
-      group.foldLeft(List.empty[BookWithPrice]) { (pricedBooks, bookId) =>
-        val alreadyPriced = pricedBooks.find(_.id == bookId)
-        val pricedBook =
-          alreadyPriced.fold(BookWithPrice.discounted(bookId)(discount(distinctBooksInGroup(group))))(_.withoutDiscount)
-        pricedBook :: pricedBooks
+  def priceBookGroup(group: List[Int]): List[BookWithPrice] =
+    group.foldLeft(List.empty[BookWithPrice]) { (pricedBooks, bookId) =>
+      val alreadyPriced = pricedBooks.find(_.id == bookId)
+      val pricedBook = alreadyPriced.fold(bookWithDiscount(bookId, discount(group.distinct.size)))(_.withoutDiscount)
+      pricedBook :: pricedBooks
     }
 
-  val groupPrice: List[Int] => Int =
-    items => priceBookGroup(items).map(_.price).sum
+  def groupPrice(items: List[Int]): Int =
+    priceBookGroup(items).map(_.price).sum
 
 }
