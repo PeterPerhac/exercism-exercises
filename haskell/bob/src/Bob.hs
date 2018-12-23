feb7478d479bf1b8cc94edc86b60839352f0f13f
Peter Perhac <perhac.peter@gmail.com>
2018-12-23 19:38:39 +0000

    this is my first working solution of Bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,9 +1,9 @@
-{-# LANGUAGE LambdaCase                #-}
+{-# LANGUAGE LambdaCase    #-}
 module Bob (responseFor) where
 
-import Data.Char (isSpace, toUpper)
+import Data.Char (isSpace, isAlpha, toUpper)
 
-data Interaction = None | ShoutingQuestions | Shouting | Questioning | General
+data Interaction = None | ShoutingQuestions | Shouting | Questioning | General deriving (Show)
 
 phrase :: Interaction -> String
 phrase = \case
@@ -16,44 +16,23 @@ phrase = \case
 trim :: String -> String
 trim = let f = reverse . dropWhile isSpace in f . f
 
-interactionType :: String -> Interaction
-interactionType input
-  | null input = None
-  | isShouting s && isQuestion s = ShoutingQuestions
-  | isQuestion s = Questioning
-  | isShouting s = Shouting
+mode :: String -> Interaction
+mode input
+  | noInput = None
+  | isShouting input && isQuestion input = ShoutingQuestions
+  | isQuestion input = Questioning
+  | isShouting input = Shouting
   | otherwise = General
   where acronyms = ["OK", "DMV"]
-        s = trim input
-        up = map toUpper s
-        shouting w = length w > 1 && up == w && not (elem w acronyms)
-        isShouting = not . null . (filter shouting) . words
-        isQuestion = ('?' == ) . last
+        noInput = null . trim $ input
+        shoutedWord w =
+          let uppercased = map toUpper w in
+            length w > 1 &&
+            uppercased == w &&
+            (not . elem uppercased $ acronyms)
+        isShouting = not . null . (filter shoutedWord) . words . filter isAlpha
+        isQuestion = ('?' == ) . last . trim
 
 responseFor :: String -> String
-responseFor = phrase . interactionType
+responseFor = phrase . mode
 
-{-
-  my scala solution
-
- object Bob {
-  def response(statement: String): String = {
-    val s = statement.trim
-    val acronyms = Set("OK", "DMV")
-    val isShouting = s
-      .split("[^a-zA-Z]+")
-      .filter { word =>
-        word.toUpperCase == word && word.length > 1 && !acronyms.contains(word.toUpperCase)
-      }
-      .nonEmpty
-    val isQuestion = s.endsWith("?")
-    (isQuestion, isShouting) match {
-      case (_, _) if s.isEmpty => "Fine. Be that way!"
-      case (true, true)        => "Calm down, I know what I'm doing!"
-      case (true, false)       => "Sure."
-      case (false, true)       => "Whoa, chill out!"
-      case _                   => "Whatever."
-    }
-  }
-}
--}
