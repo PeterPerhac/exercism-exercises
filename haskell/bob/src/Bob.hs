900577e05ed78855c6110fea8d842e4e87920716
Peter Perhac <perhac.peter@gmail.com>
2018-12-24 00:15:47 +0000

    some moving around

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -5,6 +5,9 @@ import Data.Char (isSpace, isAlpha, toUpper)
 
 data Interaction = None | ShoutingQuestions | Shouting | Questioning | General deriving (Show)
 
+responseFor :: String -> String
+responseFor = phrase . mode
+
 phrase :: Interaction -> String
 phrase = \case
   None -> "Fine. Be that way!"
@@ -13,8 +16,6 @@ phrase = \case
   Questioning -> "Sure."
   General -> "Whatever."
 
-trim :: String -> String
-trim = let f = reverse . dropWhile isSpace in f . f
 
 mode :: String -> Interaction
 mode input
@@ -23,16 +24,13 @@ mode input
   | isQuestion input = Questioning
   | isShouting input = Shouting
   | otherwise = General
-  where acronyms = ["OK", "DMV"]
-        noInput = null . trim $ input
-        shoutedWord w =
-          let uppercased = map toUpper w in
-            length w > 1 &&
-            uppercased == w &&
-            (not . elem uppercased $ acronyms)
-        isShouting = not . null . (filter shoutedWord) . words . filter isAlpha
-        isQuestion = ('?' == ) . last . trim
-
-responseFor :: String -> String
-responseFor = phrase . mode
+  where
+    trim = let f = reverse . dropWhile isSpace in f . f
+    noInput = null . trim $ input
+    shoutedWord w = let uppercased = map toUpper w in
+      length w > 1 &&
+      uppercased == w &&
+      (not . elem uppercased $ ["OK", "DMV"])
+    isShouting = not . null . (filter shoutedWord) . words . filter isAlpha
+    isQuestion = ('?' == ) . last . trim
 
