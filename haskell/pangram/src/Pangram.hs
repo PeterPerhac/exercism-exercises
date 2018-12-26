218644a2853d7d7c1b8d9464cc537b614f78e1f5
Peter Perhac <perhac.peter@gmail.com>
2018-12-26 20:49:45 +0000

    simplified after hints from mentor

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,14 +1,13 @@
 module Pangram (isPangram) where
 
 import Data.List
-import Data.Char (isAlpha, toLower)
-
+import Data.Char (toLower)
 
 isPangram :: String -> Bool
 isPangram = checkPangram []
 
 checkPangram :: [Char] -> String -> Bool
 checkPangram chars text = (null $ ['a'..'z'] \\ chars) || case text of
-    [] -> False
-    h:t -> checkPangram (union chars (filter isAlpha . map toLower $ [h])) t
+  []  -> False
+  h:t -> checkPangram (toLower h : chars) t
 
