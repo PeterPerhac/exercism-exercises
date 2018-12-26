839c2d74c1cde885034a315879a63c04c0d9537a
Peter Perhac <perhac.peter@gmail.com>
2018-12-26 13:31:03 +0000

    this passes all tests, my first cut

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,4 +1,24 @@
 module IsbnVerifier (isbn) where
 
+import Data.Maybe
+import Data.List
+import Data.Char (isDigit, digitToInt)
+
 isbn :: String -> Bool
-isbn = error "You need to implement this function."
+isbn = validIsbn . catMaybes . map charMapping
+  where charMapping 'X' = Just 10
+        charMapping c
+         | isDigit c = Just $ digitToInt c
+         | otherwise = Nothing
+
+validIsbn :: [Int] -> Bool
+validIsbn ds = checkDigitValid ds && validIsbnLength ds
+  where validIsbnLength digits = case length digits of
+          10 -> divisibleBy 11 . sum . checksum $ ds
+          _  -> False
+        checksum digits = let mult (n, rank) = (10-rank) * n in
+          map mult $ zip [10,9..] digits
+        checkDigitValid digits = let idx = elemIndex 10 digits in
+          elem idx [Nothing, Just 9]
+        divisibleBy d = (0 ==) . flip rem d
+
