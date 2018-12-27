692e42accdc067ab86693b76904efe485aa198d2
Peter Perhac <perhac.peter@gmail.com>
2018-12-27 12:37:10 +0000

    final solution, omfg

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -4,4 +4,4 @@ import Data.List
 import Data.Char (toLower)
 
 isPangram :: String -> Bool
-isPangram text = any (null . (\\) ['a'..'z']) ((map . map) toLower $ inits text)
+isPangram = null . (\\) ['a'..'z'] . map toLower
