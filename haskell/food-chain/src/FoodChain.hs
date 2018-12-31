0608d3cbc6134a9c1f1775eb15aa6c8416939ee6
Peter Perhac <perhac.peter@gmail.com>
2018-12-31 10:34:27 +0000

    adding isogram exercise and my half arsed attempt at food-chain

--- haskell/food-chain/src/FoodChain.hs
+++ haskell/food-chain/src/FoodChain.hs
@@ -1,54 +1,63 @@
+{-# LANGUAGE RecordWildCards #-}
 module FoodChain (song) where
 
+import Data.Maybe (catMaybes)
+
+data AnimalVerse = AnimalVerse {
+                   animal :: String
+                 , exclamation       :: Maybe String
+                 , swallowedEnding   :: Maybe String
+                 }
+
 song :: String
-song =
-    "I know an old lady who swallowed a fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a spider.\n\
-    \It wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a bird.\n\
-    \How absurd to swallow a bird!\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a cat.\n\
-    \Imagine that, to swallow a cat!\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a dog.\n\
-    \What a hog, to swallow a dog!\n\
-    \She swallowed the dog to catch the cat.\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a goat.\n\
-    \Just opened her throat and swallowed a goat!\n\
-    \She swallowed the goat to catch the dog.\n\
-    \She swallowed the dog to catch the cat.\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a cow.\n\
-    \I don't know how she swallowed a cow!\n\
-    \She swallowed the cow to catch the goat.\n\
-    \She swallowed the goat to catch the dog.\n\
-    \She swallowed the dog to catch the cat.\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a horse.\n\
-    \She's dead, of course!\n"
+song = concatMap stanza aAndPrev
+  where
+    aAndPrev = animals `zip` (Nothing : ((map Just . map animal) animals))
+    sheSwallowed = (++ ".") . (++) "I know an old lady who swallowed a "
+    toCatchTheOther ending a prev = "She swallowed the " ++ a ++ " to catch the " ++ prev ++ maybe "." id ending
+    perhapsShellDie = "I don't know why she swallowed the fly. Perhaps she'll die."
+    catchingLine ending a prev = (toCatchTheOther ending a) <$> prev
+    stanza (AnimalVerse{..}, prev) = unlines $ catMaybes [Just $ sheSwallowed animal, exclamation, catchingLine swallowedEnding animal prev, Just perhapsShellDie, Just ""]
+
+animals = [
+    AnimalVerse {
+      animal = "fly"
+    , exclamation = Nothing
+    , swallowedEnding = Nothing
+    },
+    AnimalVerse {
+      animal = "spider"
+    , exclamation = Just "It wriggled and jiggled and tickled inside her."
+    , swallowedEnding = Nothing
+    },
+    AnimalVerse {
+      animal = "bird"
+    , exclamation = Just "How absurd to swallow a bird!"
+    , swallowedEnding = Just " that wriggled and jiggled and tickled inside her."
+    },
+    AnimalVerse {
+      animal = "cat"
+    , exclamation = Just "Imagine that, to swallow a cat!"
+    , swallowedEnding = Nothing
+    },
+    AnimalVerse {
+      animal = "dog"
+    , exclamation = Just "What a hog, to swallow a dog!"
+    , swallowedEnding = Nothing
+    },
+    AnimalVerse {
+      animal = "goat"
+    , exclamation = Just "Just opened her throat and swallowed a goat!"
+    , swallowedEnding = Nothing
+    },
+    AnimalVerse {
+      animal = "cow"
+    , exclamation = Just "I don't know how she swallowed a cow!"
+    , swallowedEnding = Nothing
+    },
+    AnimalVerse {
+      animal = "horse"
+    , exclamation = Just "She's dead, of course!"
+    , swallowedEnding = Nothing
+    }
+    ]
