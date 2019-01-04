01444353c68e0a2f07be98001f8543c63d01863d
Peter Perhac <perhac.peter@gmail.com>
2019-01-04 19:24:26 +0000

    some improvement following mentor feedback
    will try non-recursive solution too

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -3,10 +3,9 @@ module CollatzConjecture (collatz) where
 import Control.Monad (mfilter)
 
 collatz :: Integer -> Maybe Integer
-collatz = fmap (steps 0) . mfilter (>0) . Just
-  where
-    steps n 1 = n
-    steps n num
-      | even num = steps(n+1)(num `div` 2)
-      | otherwise = steps(n+1)(3*num + 1)
+collatz = fmap steps . mfilter (>0) . Just
+  where steps 1 = 0
+        steps n
+          | even n = 1 + steps (n `quot` 2)
+          | otherwise = 1 + steps(3*n + 1)
 
