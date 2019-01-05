5f99598003befd6b966cc73d7ae7f42fb659640c
Peter Perhac <perhac.peter@gmail.com>
2019-01-05 18:16:26 +0000

    return instead Just

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -4,7 +4,7 @@ import Control.Monad (mfilter)
 import Data.List (genericLength)
 
 collatz :: Integer -> Maybe Integer
-collatz = fmap (genericLength . takeWhile (/=1) . iterate step) . mfilter (>0) . Just
+collatz = fmap (genericLength . takeWhile (/=1) . iterate step) . mfilter (>0) .return
   where step n
            | even n = n `quot` 2
            | otherwise = 3*n + 1
