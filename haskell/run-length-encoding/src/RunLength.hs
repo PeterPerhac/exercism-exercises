95b836283ddab5110d345a6d313baf210a8e3dac
Peter Perhac <perhac.peter@gmail.com>
2019-01-07 16:15:45 +0000

    first working solution (passes tests) but needs tuning

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,20 @@
 module RunLength (decode, encode) where
 
+import Data.Char
+import Control.Monad
+import Data.List
+import Data.List.Split
+import Text.Read
+
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode = concat . (map explode) . (split splitter)
+    where splitter = dropFinalBlank . keepDelimsR $ whenElt (flip elem $ supportedChars)
+          explode = join . liftM2 replicate times letter
+          times = maybe 1 id . readMaybe . takeWhile isDigit
+          letter = dropWhile isDigit
+          supportedChars = ' ' : ['a'..'z'] ++ ['A'..'Z']
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode = concat . map implode . liftM2 zip (map $ show . length) (map nub) . group
+  where implode ("1", letter) = letter
+        implode (num, letter) = num ++ letter
