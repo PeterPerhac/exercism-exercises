4e05dd1170be9638b23ce4d20bcabe60f4925340
Peter Perhac <perhac.peter@gmail.com>
2019-01-08 11:19:46 +0000

    adding rust track hello world

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,11 @@
 module DNA (toRNA) where
 
+import Control.Monad
+
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA = join . fmap (map mapping) . return
+  where mapping c = case c of 'G' -> Right 'C'
+                              'C' -> Right 'G'
+                              'T' -> Right 'A'
+                              'A' -> Right 'U'
+                              otherwise -> Left c
