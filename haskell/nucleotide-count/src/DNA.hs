953503e4fea8b5392b86f4e35e46ae727fb2373d
Peter Perhac <perhac.peter@gmail.com>
2019-01-12 21:40:46 +0000

    wip

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,11 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map)
+import Data.Map (Map, fromList)
+import Text.Read (readMaybe)
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts = fmap fromList . (fmap ((startingPoint ++ ) . map (\c -> (c, 1)))) . traverse readNucleotide
+  where startingPoint = map (\c -> (c, 0)) [A ..]
+        readNucleotide c = maybe (Left [c]) Right (readMaybe [c]::Maybe Nucleotide)
