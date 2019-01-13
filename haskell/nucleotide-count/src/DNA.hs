9d6ffc0ed6db9e73a766763b17f2e25a78ec61ee
Peter Perhac <perhac.peter@gmail.com>
2019-01-13 11:38:50 +0000

    found fromListWith and now all tests passing

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,11 +1,11 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map, fromList)
+import Data.Map (Map, fromListWith)
 import Text.Read (readMaybe)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts = fmap fromList . (fmap ((startingPoint ++ ) . map (\c -> (c, 1)))) . traverse readNucleotide
+nucleotideCounts = fmap (fromListWith (+) . (startingPoint ++ ) . map (\c -> (c, 1))) . traverse readNucleotide
   where startingPoint = map (\c -> (c, 0)) [A ..]
         readNucleotide c = maybe (Left [c]) Right (readMaybe [c]::Maybe Nucleotide)
