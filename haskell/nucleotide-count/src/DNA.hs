183696b65debb6e3e485fcb7a22745c24795d71a
Peter Perhac <perhac.peter@gmail.com>
2019-01-14 18:04:35 +0000

    improving solution with hints from mentor feedback

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,15 +1,12 @@
+{-# LANGUAGE TupleSections #-}
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map, fromListWith)
-import Text.Read (readMaybe)
-import Control.Monad
+import Text.Read (readEither)
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Read)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Bounded, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts = fmap (fromListWith (+) . (startingPoint ++ ) . map (\c -> (c, 1))) . traverse readIt
-  where startingPoint = map (\c -> (c, 0)) [A ..]
-        readIt = ap (flip maybe Right . Left . return) readNucleotide
-
-readNucleotide :: Char -> Maybe Nucleotide
-readNucleotide = readMaybe . return
+nucleotideCounts = fmap (fromListWith (+) . (startingPoint ++ ) . map ((, 1))) . traverse readNucleotide
+  where startingPoint = map ((, 0)) [minBound::Nucleotide ..]
+        readNucleotide = readEither . return
