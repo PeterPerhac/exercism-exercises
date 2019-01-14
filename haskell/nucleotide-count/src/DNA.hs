84b449295d3f7b19f5266d70ddca52138f35d321
Peter Perhac <perhac.peter@gmail.com>
2019-01-14 08:30:13 +0000

    point-free version

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -2,10 +2,14 @@ module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map, fromListWith)
 import Text.Read (readMaybe)
+import Control.Monad
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts = fmap (fromListWith (+) . (startingPoint ++ ) . map (\c -> (c, 1))) . traverse readNucleotide
+nucleotideCounts = fmap (fromListWith (+) . (startingPoint ++ ) . map (\c -> (c, 1))) . traverse readIt
   where startingPoint = map (\c -> (c, 0)) [A ..]
-        readNucleotide c = maybe (Left [c]) Right (readMaybe [c]::Maybe Nucleotide)
+        readIt = ap (flip maybe Right . Left . return) readNucleotide
+
+readNucleotide :: Char -> Maybe Nucleotide
+readNucleotide = readMaybe . return
