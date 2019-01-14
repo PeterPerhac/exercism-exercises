8bc328104b9fc785133ba539c0c504b2dfd63391
Peter Perhac <perhac.peter@gmail.com>
2019-01-14 19:00:43 +0000

    first cut at the exercise

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,7 +1,12 @@
 module Grains (square, total) where
 
-square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+import Control.Monad (mfilter)
+
+square :: Int -> Maybe Integer
+square = fmap (squares !!) . mfilter (flip elem [0..63]) . return . pred
 
 total :: Integer
-total = error "You need to implement this function."
+total = sum $ take 64 squares
+
+squares :: [Integer]
+squares = iterate (2*) 1
