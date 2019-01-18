96d0dbc14118a2b7465f6342423d8555166efacc
Peter Perhac <perhac.peter@gmail.com>
2019-01-18 11:16:31 +0000

    i think this implementation is safer

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -2,13 +2,11 @@ module Grains (square, total) where
 
 import Control.Monad
 import Data.Bits (shiftL)
-import Data.Maybe (mapMaybe)
-
 
 square :: Int -> Maybe Integer
 square = fmap (shiftL 1 . pred) . mfilter inRange . return
   where inRange = liftM2 (&&) (> 0) (<= 64)
 
 total :: Integer
-total = sum . mapMaybe square $ [1..64]
+total = maybe 0 sum $ traverse square [1..64]
 
