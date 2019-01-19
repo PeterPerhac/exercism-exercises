e2175e33ab01bdddf5f347ee0544cf1120869ad1
Peter Perhac <perhac.peter@gmail.com>
2019-01-19 21:34:44 +0000

    first stab at leap year exercism - purescript

--- purescript/leap/src/Leap.purs
+++ purescript/leap/src/Leap.purs
@@ -1,1 +1,7 @@
 module Leap where
+
+import Prelude
+
+isLeapYear :: Int -> Boolean
+isLeapYear y = divisibleBy 4 && (divisibleBy 400 || not (divisibleBy 100))
+  where divisibleBy = (0 == _) <<< (y `mod` _)
