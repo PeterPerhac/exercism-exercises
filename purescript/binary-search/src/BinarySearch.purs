647ed54d840bf6911ba911f24a7732bce76e2ca0
Peter Perhac <perhac.peter@gmail.com>
2019-01-20 13:20:58 +0000

    wip - compiles but far from working

--- purescript/binary-search/src/BinarySearch.purs
+++ purescript/binary-search/src/BinarySearch.purs
@@ -1,3 +1,20 @@
 module BinarySearch
   ( find
   ) where
+
+import Prelude
+import Data.Maybe
+import Data.Int (quot)
+import Data.Array (length)
+
+
+find :: Int -> Array Int -> Maybe Int
+find x xs = find' x xs (length xs `quot` 2)
+
+find' :: Int -> Array Int -> Int -> Maybe Int
+find' x [] _ = Nothing
+find' x xs idx
+  | [x] == xs = Just idx
+  | otherwise = Just idx
+
+
