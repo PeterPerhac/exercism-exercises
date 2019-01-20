88aabafbe85639a95e71687e101101b4cb26ad4b
Peter Perhac <perhac.peter@gmail.com>
2019-01-20 12:50:45 +0000

    first stab at RNATranscription exercism

--- purescript/rna-transcription/src/RNATranscription.purs
+++ purescript/rna-transcription/src/RNATranscription.purs
@@ -1,3 +1,17 @@
-module RNATranscription
-  ( toRNA
-  ) where
+module RNATranscription (
+ toRNA
+) where
+
+import Prelude ((<$>), (<<<))
+import Data.Maybe
+import Data.Traversable (traverse)
+import Data.String.CodeUnits (fromCharArray, toCharArray)
+
+toRNA :: String -> Maybe String
+toRNA = (fromCharArray <$> _) <<< (traverse transcribe) <<< toCharArray
+  where transcribe = case _ of
+                         'G' -> Just 'C'
+                         'C' -> Just 'G'
+                         'T' -> Just 'A'
+                         'A' -> Just 'U'
+                         c   -> Nothing
