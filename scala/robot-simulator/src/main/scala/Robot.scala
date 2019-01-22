6f9ebfb5319b015f66386648a4df8e79933ff3a9
Peter Perhac <perhac.peter@gmail.com>
2019-01-22 22:04:40 +0000

    my first stab at the RobotSimulator exercism

---
+++ scala/robot-simulator/src/main/scala/Robot.scala
@@ -0,0 +1,41 @@
+sealed abstract class Bearing(xAdv: Int, yAdv: Int){
+  def left: Bearing
+  def right: Bearing
+  def advance(cs:(Int, Int)): (Int, Int) = { val (x,y) = cs; (x+xAdv, y+yAdv) }
+}
+object Bearing{
+  case object West extends Bearing(-1, 0){ override def left = South; override def right = North }
+  case object East extends Bearing(1, 0){ override def left = North; override def right = South }
+  case object South extends Bearing(0, -1){ override def left = East; override def right = West}
+  case object North extends Bearing(0, 1){ override def left = West; override def right = East }
+}
+
+sealed abstract class Instruction(val code: Char)
+object Instruction {
+  case object Left extends Instruction('L')
+  case object Right extends Instruction('R')
+  case object Advance extends Instruction('A')
+
+  def resolveInstructionCode(code: Char): Option[Instruction] =
+    List(Left, Right, Advance).find(_.code == code)
+
+}
+
+final case class Robot(bearing: Bearing, coordinates: (Int, Int)) {
+  import Instruction._
+
+  lazy val advance: Robot = copy(coordinates=bearing.advance(coordinates))
+  lazy val turnLeft: Robot = copy(bearing=bearing.left)
+  lazy val turnRight: Robot = copy(bearing=bearing.right)
+
+  def simulate(instructions: String): Robot = {
+    instructions.flatMap(resolveInstructionCode).foldLeft(this){ (robot, instruction) =>
+      instruction match {
+        case Left => robot.turnLeft
+        case Right => robot.turnRight
+        case Advance => robot.advance
+      }
+    }
+  }
+
+}
