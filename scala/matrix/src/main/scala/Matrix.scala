df81c61b9454d817645f7e7769b6463f8239c079
Peter Perhac <perhac.peter@gmail.com>
2019-01-24 11:19:30 +0000

    following mentor feedback

--- scala/matrix/src/main/scala/Matrix.scala
+++ scala/matrix/src/main/scala/Matrix.scala
@@ -2,11 +2,11 @@ import scala.util.Try
 
 final case class Matrix(matrixDefinition: String){
 
-  val rows: Vector[Vector[Int]] =
-    matrixDefinition.split("""\n+""").toVector.map{ line =>
+  private val rows: Vector[Vector[Int]] =
+    matrixDefinition.lines.toVector.map{ line =>
       line.split("""\s+""").toVector.flatMap(s => Try(s.toInt).toOption)
     }
-  val columns: Vector[Vector[Int]] = rows.transpose.toVector
+  private val columns: Vector[Vector[Int]] = rows.transpose.toVector
 
   def column(idx: Int): Vector[Int] = columns(idx)
   def row(idx: Int): Vector[Int] = rows(idx)
