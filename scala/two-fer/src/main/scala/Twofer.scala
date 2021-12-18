fe84db94afdf1f108f64f78554bef39011f8150b
Peter Perhac <PeterPerhac@users.noreply.github.com>
2021-12-18 13:59:19 +0000

    twofer solution

--- scala/two-fer/src/main/scala/Twofer.scala
+++ scala/two-fer/src/main/scala/Twofer.scala
@@ -1,3 +1,10 @@
 object Twofer {
-  def twofer(name: String): String = ???
+  def twofer(name: String = ""): String = {
+    val theName = name match {
+      case empty if name.trim().isEmpty => "you"
+      case theName => theName
+    }
+
+    s"One for $theName, one for me."
+  }
 }
