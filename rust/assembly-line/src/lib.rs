614ab03dad307076f674119d92c89217f0c22753
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-08 22:54:56 +0000

    added more exercises, and solved one

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {speed}")
+    let success_rate: f64 = match speed {
+        0_u8 => 0.0,
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        11_u8..=u8::MAX => todo!(),
+    };
+    ((speed as u64 * 221) as f64) * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {speed}")
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
