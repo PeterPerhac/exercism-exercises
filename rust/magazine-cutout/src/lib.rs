6a08771da73e78460615a7025aa15a2c3956fa99
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-22 19:39:22 +0000

    magazine cutout exercise

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,9 +1,9 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut m_hash: HashMap<&&str, i32> = HashMap::new();
+    for w in magazine { *m_hash.entry(w).or_default() += 1; }
+    let mut n_hash: HashMap<&&str, i32> = HashMap::new();
+    for w in note { *n_hash.entry(w).or_default() += 1; }
+    n_hash.iter().all(|(w, c)| m_hash.entry(w).or_default() >= &mut c.clone())
 }
