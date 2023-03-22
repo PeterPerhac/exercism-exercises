7d295ab5fe139e00c56e011ef4c596f1efde28af
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-22 14:52:37 +0000

    RPG implemented

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -6,10 +6,28 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        if self.health > 0 {
+            None
+        } else {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {mana_cost}")
+        self.health = if self.level < 10 {
+            self.health.max(mana_cost) - mana_cost
+        } else {
+            self.health
+        };
+        self.mana = self.mana.map(|m| if m >= mana_cost { m - mana_cost } else { m });
+        if let Some(mana) = self.mana {
+            if mana >= mana_cost {
+                mana_cost * 2
+            } else { 0 }
+        } else { 0 }
     }
 }
