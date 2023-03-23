c14105a7ce0d9259825b6c32116628f1701c979c
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-23 10:19:18 +0000

    final iteration on rpn calculator, adding binary serach exercise

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -8,24 +8,22 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    use CalculatorInput::*;
-
     let mut stack: Vec<i32> = Vec::new();
+
+    fn calc(stack: &mut Vec<i32>, op: fn(i32, i32) -> i32) -> Option<()> {
+        let b = stack.pop()?;
+        let a = stack.pop()?;
+        stack.push(op(a, b));
+        Some(())
+    }
 
     for input in inputs {
         match input {
-            Value(n) => stack.push(*n),
-            op @ (Add | Subtract | Multiply | Divide) => {
-                let b = stack.pop()?;
-                let a = stack.pop()?;
-                stack.push(match op {
-                    Add => a + b,
-                    Subtract => a - b,
-                    Multiply => a * b,
-                    Divide => a / b,
-                    _ => panic!("can't happen")
-                });
-            }
+            CalculatorInput::Value(n) => stack.push(*n),
+            CalculatorInput::Add => calc(&mut stack, std::ops::Add::add)?,
+            CalculatorInput::Subtract => calc(&mut stack, std::ops::Sub::sub)?,
+            CalculatorInput::Multiply => calc(&mut stack, std::ops::Mul::mul)?,
+            CalculatorInput::Divide => calc(&mut stack, std::ops::Div::div)?,
         }
     }
     if stack.len() == 1 { stack.pop() } else { None }
