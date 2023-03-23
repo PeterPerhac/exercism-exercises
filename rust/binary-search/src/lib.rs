bde106a726f9e8770aab0915170cdc3efb4e726b
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-23 23:34:35 +0000

    solution to binary search

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -3,17 +3,17 @@ use tailcall::tailcall;
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
     #[tailcall]
     fn recurse(from: usize, to: usize, a: &[i32], k: i32) -> Option<usize> {
-        if to == from {
-            if a[from] == k { Some(from) } else { None }
+        if from == to {
+            Some(from).filter(|_| a[from] == k )
         } else {
-            let mid = (from + to) / 2;
-            let mid_value = a[mid];
+            let mid_index = (from + to) / 2;
+            let mid_value = a[mid_index];
             if mid_value == k {
-                Some(mid)
+                Some(mid_index)
             } else {
                 recurse(
-                    if mid_value < k { mid + 1 } else { from },
-                    if mid_value < k { to } else { mid },
+                    if mid_value < k { mid_index + 1 } else { from },
+                    if mid_value < k { to } else { mid_index },
                     a,
                     k,
                 )
