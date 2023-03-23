fa68b1582bd5d53787641b03b00cb7947ccffb23
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-23 15:34:09 +0000

    solve binary search

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,5 +1,25 @@
+use tailcall::tailcall;
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+    #[tailcall]
+    fn recurse(from: usize, to: usize, a: &[i32], k: i32) -> Option<usize> {
+        if to == from {
+            if a[from] == k { Some(from) } else { None }
+        } else {
+            let mid = (from + to) / 2;
+            let mid_value = a[mid];
+            if mid_value == k {
+                Some(mid)
+            } else {
+                recurse(
+                    if mid_value < k { mid + 1 } else { from },
+                    if mid_value < k { to } else { mid },
+                    a,
+                    k,
+                )
+            }
+        }
+    }
+    if array.is_empty() { return None; }
+    recurse(0, array.len() - 1, array, key)
 }
