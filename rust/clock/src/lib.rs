4a661683c88f7aed7213f7f60a2b8d1ab49ca253
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-24 16:43:16 +0000

    implementing automated exercism comments

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -4,19 +4,16 @@ const MINUTES_PER_DAY: i32 = 24 * 60;
 
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Display for Clock {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+        write!(f, "{:0>2}:{:0>2}", self.minutes / 60, self.minutes % 60)
     }
 }
 
 impl Clock {
-    fn to_minutes(self: &Self) -> i32 { self.hours * 60 + self.minutes }
-
     fn adjust_minutes(input: i32) -> i32 {
         let days = if input >= MINUTES_PER_DAY {
             input / MINUTES_PER_DAY
@@ -26,16 +23,11 @@ impl Clock {
         input - (days * MINUTES_PER_DAY)
     }
 
-    fn from_minutes(n: i32) -> Clock {
-        let adjusted_minutes = Self::adjust_minutes(n);
-        Clock { hours: adjusted_minutes / 60, minutes: adjusted_minutes % 60 }
-    }
-
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Self::from_minutes(hours * 60 + minutes)
+        Clock { minutes: Self::adjust_minutes(hours * 60 + minutes) }
     }
 
     pub fn add_minutes(self: &Self, minutes: i32) -> Self {
-        Self::from_minutes(self.to_minutes() + minutes)
+        Clock { minutes: Self::adjust_minutes(self.minutes + minutes) }
     }
 }
